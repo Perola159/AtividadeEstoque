0b2399b5b744b08c2fa90f07fd049f16fc2a467f
Perola159 <167914732+Perola159@users.noreply.github.com>
2024-07-05 17:02:49 -0300

    atualizar

--- AtividadeEstoque/Program.cs
+++ AtividadeEstoque/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AtividadeEstoque;
+using AtividadeEstoqueDolarPagamneto;
 
 //ATIVIDADE 1
 //estoque est = new estoque();
@@ -12,10 +13,21 @@
 
 
 //ATIVIDADE 3
-Pagamento pag = new Pagamento();
-pag.IdentVende();
-pag.PrecoPeca();
-pag.QntdVendida();
-pag.Codpeca();
-pag.Valortotal();
-pag.ComissaoVenda();
+//Pagamento pag = new Pagamento();
+//pag.IdentVende();
+//pag.PrecoPeca();
+//pag.QntdVendida();
+//pag.Codpeca();
+//pag.Valortotal();
+//pag.ComissaoVenda();
+
+
+//ATIVIDADE 4 
+//VariavelAB var = new VariavelAB();
+//var.AtribuindoValorVariavel();
+//var.TrocaValor();
+
+
+//Atividade 5 
+VariavelAB vari = new VariavelAB();
+vari.Modulo();
---
+++ AtividadeEstoque/VariavelAB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeEstoqueDolarPagamneto
+{
+    public class VariavelAB
+    {
+        public int VarA { get; set; }
+        public int VarB { get; set; }
+
+        public void AtribuindoValorVariavel()
+        {
+            Console.WriteLine("Digite um valor inteiro A:");
+            VarA = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite outro valor inteiro B:");
+            VarB = int.Parse(Console.ReadLine());
+        }
+
+        public void TrocaValor()
+        {
+            int VarAaux = VarA;
+            int VarBaux = VarB;
+
+            VarA = VarBaux;
+            VarB = VarAaux;
+
+            Console.WriteLine($"O valor de A é: {VarA}");
+            Console.WriteLine($"O valor de B é: {VarB}");
+        }
+    }
+}
